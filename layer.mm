4810122ab3986f8114668a05ef041aea9e2879ad
jasper <jasperkoning94@hotmail.com>
2021-08-10 09:41:42 +0200

    cleanup

--- layer.mm
+++ layer.mm
@@ -1,17 +1,45 @@
-#include "SocketServer.h"
+#include "Socket.h"
+#include <Foundation/Foundation.h>
+#include <LightMessaging/LightMessaging.h>
+#include <LightMessaging/LM.h>
+#include <pthread.h>
+
+void *readFromSocket(void *data)
+{
+	Socket *socket(reinterpret_cast<Socket *>(data));
+	socket->read();
+	delete socket;
+	return NULL;
+}
+
+// onPID creates a socket /tmp/.jk.<pid> on each received PID
+static void onPID(CFMachPortRef port, LMMessage *message, CFIndex size, void *info)
+{
+	void *data = LMMessageGetData(message);
+	auto path = (const char *)((const UInt8*)data ?: (const UInt8 *)&data);
+//	int pid = atoi(path);
+	Socket *socket = new Socket();
+	socket->connect(path);
+	pthread_t thread;
+	pthread_create(&thread, NULL, &readFromSocket, socket);
+}
 
 extern "C"
 __attribute__ ((visibility("default")))
 void launchSocketServer(char const *name)
 {
-	SocketServer::launch(name);
+	LMStartService((char *)name, CFRunLoopGetMain(), (CFMachPortCallBack)onPID);
 }
 
 extern "C"
 __attribute__ ((visibility("default")))
 int createSocket(char const *name, int pid)
 {
-	SocketServer server(name);
-	return server.createSocket(pid);
+	char path[30];
+	sprintf(path, "/tmp/.jk.%d", pid);
+	// create socket from remote server
+	jkmsgsend(name, 0x1111, path, strlen(path) + 1);
+	// create accepted socket
+	return Socket::accept(path);
 }
 
--- socket.c
+++ socket.c
@@ -1,10 +1,11 @@
 #include "socketserver.h"
 #include <stdio.h>
 //#include <LightMessaging/LightMessaging.h>
+#include <unistd.h>
 
 int main()
 {
-	printf("%d", createSocket("jk.socket"));
+	printf("%d", createSocket("jk.socket", getpid()));
 	// onPID creates a socket /tmp/.jk.<pid> on each received PID
 	launchSocketServer("jk.socket");
 }
